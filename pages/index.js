af6f3a2b245d6624ac731e0b7f409af051eeb57a
Diogo Santiago <diogo@pentagrama.com.br>
2021-01-27 00:36:09 -0300

    aula 02

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,26 @@ export default function Home() {
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-teste
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  // console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar&nbsp;
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
